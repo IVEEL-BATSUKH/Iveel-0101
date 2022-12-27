f252771afd3c126cce4e201427e241b0546bdfc5
IVEEL-BATSUKH <105977966+IVEEL-BATSUKH@users.noreply.github.com>
2022-12-27 11:39:10 +0900

    1

--- content_script.js
+++ content_script.js
@@ -1,10 +1,17 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message,sender,sendresponse)
+// chrome.runtime.onMessage.addListener(gotMessage);
+// function gotMessage(message,sender,sendresponse)
+// {
+// 	console.log(message.txt);
+// 	let paragraphs = document.getElementsByTagName("p");
+// 	for(elt of paragraphs)
+// 	{
+// 		elt.style['background-color'] = '#40D51F';
+// 	}
+// }
+
+// console.log(message.txt);
+let paragraphs = document.getElementsByTagName("p");
+for(elt of paragraphs)
 {
-	console.log(message.txt);
-	let paragraphs = document.getElementsByTagName("p");
-	for(elt of paragraphs)
-	{
-		elt.style['background-color'] = '40D51F';
-	}
+	elt.style['background-color'] = '#40D51F';
 }
